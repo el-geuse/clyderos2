67daaa9eddddd71df71a377b8cc3cd993aea5817
Peter Lacaze <lacazepk@gmail.com>
2024-01-21 13:22:57 +0000

    Reformatted driver package to python

---
+++ clyde_driver/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'clyde_driver'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='GroupC',
+    maintainer_email='peter.lacaze.2019@uni.strath.ac.uk',
+    description='Package holding all resource required for communicating with and on the bittle',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
