c29f336dd5e161af72bf7be1dff1320539937d6d
nssp lab <peter.lacaze.2019@uni.strath.ac.uk>
2024-03-06 11:49:33 +0000

    Changes for test script to work

--- .github/actions/lint/run.sh
+++ .github/actions/lint/run.sh
@@ -5,7 +5,7 @@ set -e
 
 if [ "$LINTER" == "flake8" ]; then
     # Ignores some additional rules that come with ROS 2 files (E111, E114)
-    ament_flake8 --config .vscode/.flake8 src/
+    ament_flake8 --config src/.flake8 src/
 else
     # Replace this line with the command you want to run for other values of LINTER
     ament_${LINTER} src/
