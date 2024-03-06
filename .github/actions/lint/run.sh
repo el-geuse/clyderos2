b626feef5451115567c6e2e98d18c0d518bbfd87
nssp lab <peter.lacaze.2019@uni.strath.ac.uk>
2024-03-06 13:59:42 +0000

    flake8 error ignore integrated into colcon test

--- .github/actions/lint/run.sh
+++ .github/actions/lint/run.sh
@@ -2,11 +2,4 @@
 set -e
 
 ./setup.sh
-
-if [ "$LINTER" == "flake8" ]; then
-    # Ignores some additional rules that come with ROS 2 files (E111, E114)
-    ament_flake8 --config .flake8 src/
-else
-    # Replace this line with the command you want to run for other values of LINTER
-    ament_${LINTER} src/
-fi
+ament_${LINTER} src/
