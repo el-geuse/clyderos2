8c0b27b7e68463db46fc760a7ed84aa90bc748b8
nssp lab <peter.lacaze.2019@uni.strath.ac.uk>
2024-03-06 11:02:28 +0000

    Configuration file no longer being applied to all linters

--- .github/actions/lint/run.sh
+++ .github/actions/lint/run.sh
@@ -2,4 +2,11 @@
 set -e
 
 ./setup.sh
-ament_${LINTER} --config .vscode/.flake8 src/
+
+if [ "$LINTER" == "flake8" ]; then
+    # Ignores some additional rules that come with ROS 2 files (E111, E114)
+    ament_flake8 --config .vscode/.flake8 src/
+else
+    # Replace this line with the command you want to run for other values of LINTER
+    ament_${LINTER} src/
+fi
